05a26f70f0b5aaeee7a461955729fab5e231cf44
Zowhy <dtrovato892@gmail.com>
2024-11-08 10:40:57 -0300

    commit inicial

---
+++ Trabalho Renato - Copia/sw.js
@@ -0,0 +1,23 @@
+
+let cacheName = "Bella Pasta";
+let filesToCache = [
+  "/", "/index.html", "/cardapioM.html", "/cardapioD.html", "/cardapioC.html", "/js/main.js", "/css/style.css", "/css/cardapio.css", "/alfredo.jpg", "/bife.jpg", "/bolonhesa.jpg", "/brigadeiro.jpg", "/costela.jpg", "/estrogonofe.jpg", "/mousse.jpg", "/pesto.jpg", "/pudim.jpg",
+];
+
+/* Instalação do Service Worker e cache dos arquivos */
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Interceptação de requisições e resposta com cache ou rede */
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
